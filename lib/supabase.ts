5da60f52c61a5d3f1924fa83efd1b1b3267631be
Ankush Sarkar <45667865+AnkushSarkar10@users.noreply.github.com>
2024-03-26 21:59:51 -0400

    added deps for jest testing, added supabase setup code (#3)

---
+++ lib/supabase.ts
@@ -0,0 +1,29 @@
+import { AppState } from 'react-native'
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.YOUR_REACT_NATIVE_SUPABASE_URL!
+const supabaseAnonKey = process.env.YOUR_REACT_NATIVE_SUPABASE_ANON_KEY!
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
