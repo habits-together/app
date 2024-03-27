5b863bec9c70cbc56c642b5dfeae84909c743b49
Geon Youn <38792784+geon-youn@users.noreply.github.com>
2024-03-26 20:49:38 -0400

    create bottom menu (#2)
    add the three main tabs: Habits, Friends, and Settings
    add icons for each of the tabs

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,12 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function HabitsTab() {
+export default function SettingsTab() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Habits</Text>
+      <Text style={styles.title}>Settings</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/settings.tsx" />
     </View>
   );
 }
