2802690e88da8b720d3cc07d40dadc5c6aeecc2c
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2024-03-28 18:05:25 -0400

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,11 @@
-import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { HabitCalender } from '@/components/HabitCalender';
 
 export default function HabitsTab() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Habits</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className='flex-1 bg-grey-50 p-4'>
+      <HabitCalender/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
