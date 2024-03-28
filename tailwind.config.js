f5510d262fa699f5d0a8f8c6197aa8930a35ef58
owengretzinger <owengretzinger@gmail.com>
2024-03-28 14:54:02 -0400

    Import color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const colors = require("./constants/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}','./components/**/*.{js,jsx,ts,tsx}'],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: colors,
     extend: {},
   },
   plugins: [],
