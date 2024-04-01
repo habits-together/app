e246ec6d320d30373b47b82731d2a81d0f9688c0
owengretzinger <owengretzinger@gmail.com>
2024-04-01 01:26:30 -0400

    Refactor settings & add theme setting

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,8 +1,7 @@
-import { Text, View, TouchableOpacity, useColorScheme } from "react-native";
+import { useColorScheme, ScrollView } from "react-native";
 import {
   IconAward,
   IconCategory2,
-  IconChevronRight,
   IconFileText,
   IconKey,
   IconLock,
@@ -12,12 +11,15 @@ import {
   IconSun,
 } from "@tabler/icons-react-native";
 import colors from "@/constants/colors";
-import Divider from "@/components/Divider";
 import { resetNavigationStack } from "@/lib/resetNavigationStack";
+import { router } from "expo-router";
+import {
+  SettingsContainer,
+  SettingsList,
+} from "@/components/SettingsComponents";
 
-export default function SettingsTab() {
+export default function Settings() {
   let colorScheme = useColorScheme();
-
   const iconProps = {
     size: 24,
     color: colorScheme === "dark" ? colors.white : colors.black,
@@ -25,128 +27,77 @@ export default function SettingsTab() {
   };
 
   return (
-    <View className="flex-1 p-4 bg-grey-50 dark:bg-black">
-      <Section
-        addMargin={false}
+    <SettingsContainer>
+      <SettingsList
         settings={[
-          <Setting
-            icon={<IconSun {...iconProps} />}
-            title="Theme"
-            onPress={() => {}}
-          />,
-          <Setting
-            icon={<IconCategory2 {...iconProps} />}
-            title="Widget settings"
-            onPress={() => {}}
-          />,
-          <Setting
-            icon={<IconPremiumRights {...iconProps} />}
-            title="Purchase unlimited access"
-            onPress={() => {}}
-          />,
+          {
+            icon: <IconSun {...iconProps} />,
+            title: "Theme",
+            onPress: () => {
+              router.push("/specificsettings/theme");
+            },
+          },
+          {
+            icon: <IconCategory2 {...iconProps} />,
+            title: "Widget settings",
+            onPress: () => {
+              router.push("/specificsettings/widgetsettings");
+            },
+          },
+          {
+            icon: <IconPremiumRights {...iconProps} />,
+            title: "Purchase unlimited access",
+            onPress: () => {},
+          },
         ]}
       />
-      <Section
+      <SettingsList
         settings={[
-          <Setting
-            icon={<IconSend {...iconProps} />}
-            title="Send feedback or report a bug"
-            onPress={() => {}}
-          />,
-          <Setting
-            icon={<IconSparkles {...iconProps} />}
-            title="Rate the app"
-            onPress={() => {}}
-          />,
+          {
+            icon: <IconSend {...iconProps} />,
+            title: "Send feedback or report a bug",
+            onPress: () => {},
+          },
+
+          {
+            icon: <IconSparkles {...iconProps} />,
+            title: "Rate the app",
+            onPress: () => {},
+          },
         ]}
       />
-      <Section
+      <SettingsList
         settings={[
-          <Setting
-            icon={<IconAward {...iconProps} />}
-            title="Credits"
-            onPress={() => {}}
-          />,
-          <Setting
-            icon={<IconLock {...iconProps} />}
-            title="Privacy policy"
-            onPress={() => {}}
-          />,
-          <Setting
-            icon={<IconFileText {...iconProps} />}
-            title="Terms of use"
-            onPress={() => {}}
-          />,
+          {
+            icon: <IconAward {...iconProps} />,
+            title: "Credits",
+            onPress: () => {},
+          },
+
+          {
+            icon: <IconLock {...iconProps} />,
+            title: "Privacy policy",
+            onPress: () => {},
+          },
+
+          {
+            icon: <IconFileText {...iconProps} />,
+            title: "Terms of use",
+            onPress: () => {},
+          },
         ]}
       />
-      <Section
+      <SettingsList
         settings={[
-          <Setting
-            icon={<IconKey {...iconProps} />}
-            title="Go to sign in"
-            onPress={() => {
+          {
+            icon: <IconKey {...iconProps} />,
+            title: "Go to sign in",
+            onPress: () => {
               resetNavigationStack("/(auth)/signin");
-            }}
-          />,
+            },
+          },
         ]}
       />
-    </View>
-  );
-}
-
-function Section({
-  settings,
-  addMargin = true,
-}: {
-  settings: React.ReactNode[];
-  addMargin?: boolean;
-}) {
-  return (
-    <View
-      className={`border border-grey-200 rounded-xl overflow-hidden ${
-        addMargin && "mt-6"
-      }`}
-    >
-      {/* settings with Divider in between each one */}
-      {settings.map((setting, index) => (
-        <View key={index}>
-          {setting}
-          {index < settings.length - 1 && <Divider />}
-        </View>
-      ))}
-    </View>
-  );
-}
-
-function Setting({
-  icon,
-  title,
-  onPress,
-}: {
-  icon: React.ReactNode;
-  title: string;
-  onPress: () => void;
-}) {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      className="flex-row items-center p-4 bg-white dark:bg-grey-800"
-    >
-      {icon}
-      <Text
-        numberOfLines={1}
-        adjustsFontSizeToFit={true}
-        className="flex-1 pl-2 text-base font-medium dark:text-white"
-      >
-        {title}
-      </Text>
-      <Chevron />
-    </TouchableOpacity>
-  );
-}
-
-function Chevron() {
-  return (
-    <IconChevronRight size={24} color={colors.grey["400"]} strokeWidth={2} />
+    </SettingsContainer>
   );
 }
---
+++ app/specificsettings/_layout.tsx
@@ -0,0 +1,39 @@
+import { router, Stack } from "expo-router";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import {
+  IconChevronLeft,
+} from "@tabler/icons-react-native";
+import { useColorScheme } from "react-native";
+import colors from "@/constants/colors";
+import Icon from "@/components/Icon";
+import { Text, View } from "react-native";
+
+export default function SettingsLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerTitle: (props) => (
+          <Text className="text-3xl font-bold my-auto text-center text-black dark:text-white">
+            {props.children}
+          </Text>
+        ),
+        
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+            <Icon icon={IconChevronLeft} size={32} />
+          </TouchableOpacity>
+        ),
+        headerStyle: {
+          backgroundColor: colorScheme === "dark" ? colors.stone.base : colors.stone["50"],
+        },
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen name="theme" options={{ title: "Theme" }} />
+      <Stack.Screen name="widgetsettings" options={{ title: "Widget Settings" }} />
+    </Stack>
+  );
+}
