f623b82301a778b2f5a1fc229ae44a7d579737e1
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2024-04-25 18:23:42 -0400

    added HabitInvite Component to display someone inviting someone for a habit

--- src/app/(tabs)/friends.tsx
+++ src/app/(tabs)/friends.tsx
@@ -1,13 +1,20 @@
 import FriendSearchBar from "@/src/components/FriendSearchBar";
+import HabitInvite from "@/src/components/HabitInvite";
 import { Text, View } from "@/src/components/Themed";
+import { IconMusic } from "@tabler/icons-react-native";
 
 export default function FriendsTab() {
   return (
     <View className="flex-1 p-4">
       <Text className="text-2xl font-bold">New friend requests</Text>
       <Text className="text-2xl font-bold">New habit invite</Text>
+
+      <View className="">
+        <HabitInvite title="Play Guitar" color="purple" icon={IconMusic} numberOfParticipants={3} inviterUserName="blaze_kush" />
+      </View>
+
       <Text className="text-2xl font-bold">My friends</Text>
-      <FriendSearchBar/>
+      <FriendSearchBar />
     </View>
   );
 }
