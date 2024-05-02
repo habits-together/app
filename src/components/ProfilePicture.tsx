0f9c5c9ab639ce0300e08e7b470114b86063f32b
owengretzinger <owengretzinger@gmail.com>
2024-05-02 14:51:00 -0400

    Improve profile picture logic

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,31 +1,27 @@
 import { Image } from "react-native";
 
 export default function SmallProfilePicture({ picUrl }: { picUrl: string }) {
-  if (picUrl === "") {
-    return (
-      <Image
-        className="h-12 w-12 rounded-full bg-stone-200"
-        source={require('../../assets/images/default_user.png')}
-      />
-    );
-  } else {
-    return (
-      <Image
-        className="h-12 w-12 rounded-full bg-stone-200"
-        source={{ uri: picUrl }}
-      />
-    );
-  }
+  return (
+    <Image
+      className="h-12 w-12 rounded-full bg-stone-200"
+      source={
+        picUrl
+          ? { uri: picUrl }
+          : require("../../assets/images/default_user.png")
+      }
+    />
+  );
 }
 
 export function BigProfilePicture({ picUrl }: { picUrl: string }) {
-  if (picUrl == "") {
-    return (
-      <Image className="h-24 w-24 rounded-[20px]" source={require('../../assets/images/default_user.png')} />
-    );
-  } else {
-    return (
-      <Image className="h-24 w-24 rounded-[20px]" source={{ uri: picUrl }} />
-    );
-  }
+  return (
+    <Image
+      className="h-24 w-24 rounded-[20px]"
+      source={
+        picUrl
+          ? { uri: picUrl }
+          : require("../../assets/images/default_user.png")
+      }
+    />
+  );
 }
