2b03771969c1722cde7912f029c5ade923a8ea74
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2024-05-03 23:56:07 -0400

    donezo

--- src/app/(auth)/signin.tsx
+++ src/app/(auth)/signin.tsx
@@ -47,110 +47,103 @@ import { resetNavigationStack } from "@/src/lib/resetNavigationStack";
 export default function Signin() {
   const { colorScheme } = useColorScheme();
   const { width } = Dimensions.get("window");
-  function signInWithGoogle() { }
-  function signInWithApple() { }
   const [email, setEmail] = useState("test@gmail.com");
   const [password, setPassword] = useState("password");
   const [loading, setLoading] = useState(false);
 
+  async function signInWithGoogle() {
+    resetNavigationStack("/");
+  }
+  async function signInWithApple() {
+    resetNavigationStack("/");
+  }
   async function signInWithEmail() {
-    setLoading(true);
+    resetNavigationStack("/");
+  }
 
-    // see if the account exists
-
-    setLoading(false);
-
-    // they previously created an account and are logging in
-    if (true) {
-      resetNavigationStack("/");
-    }
-    // they are creating an account for the first time
-    else {
-      router.push("/createprofile");
-    }
-
-    function TutorialItem({ item }: { item: { pageNum: string; text: string } }) {
-      return (
-        <View
-          className="m-0 items-center justify-center p-0"
-          style={{
-            width: width,
-          }}
-        >
-          <Icon icon={IconPhotoScan} size={270} strokeWidth={0.8} />
-          <Text className="px-2 text-center text-3xl font-bold">{item.text}</Text>
-        </View>
-      );
-    }
-
-    const tutorialData = [
-      { pageNum: "1", text: "Create habits and keep track of completions" },
-      { pageNum: "2", text: "Add friends and do habits together" },
-      { pageNum: "3", text: "Help each other reach your goals" },
-    ];
-    const scrollX = useRef<Animated.Value>(new Animated.Value(0)).current;
-
+  function TutorialItem({ item }: { item: { pageNum: string; text: string } }) {
     return (
-      <View className="flex-1 items-center pt-14">
-        <View className="w-11/12 flex-row items-center justify-between pb-5">
-          <View className="flex-row items-center">
-            {/* <Image
+      <View
+        className="m-0 items-center justify-center p-0"
+        style={{
+          width: width,
+        }}
+      >
+        <Icon icon={IconPhotoScan} size={270} strokeWidth={0.8} />
+        <Text className="px-2 text-center text-3xl font-bold">{item.text}</Text>
+      </View>
+    );
+  }
+
+  const tutorialData = [
+    { pageNum: "1", text: "Create habits and keep track of completions" },
+    { pageNum: "2", text: "Add friends and do habits together" },
+    { pageNum: "3", text: "Help each other reach your goals" },
+  ];
+  const scrollX = useRef<Animated.Value>(new Animated.Value(0)).current;
+
+  return (
+    <View className="flex-1 items-center pt-14">
+      <View className="w-full px-4 flex-row items-center justify-between pb-5">
+        <View className="flex-row items-center">
+          {/* <Image
             className="h-6 w-6 rounded-[20px]"
             source={require("../../../assets/images/Logo.svg")}
           /> */}
-            <Logo height={24} width={24} />
-            <Text className="ml-2 text-2xl font-bold">Habit Together</Text>
-          </View>
-          <Pressable className="rounded-2xl bg-stone-200 px-4 py-1 dark:border-2 dark:border-stone-300 dark:bg-stone-800">
-            <Text className="text-base font-semibold">Skip</Text>
-          </Pressable>
+          <Logo height={24} width={24} />
+          <Text className="ml-2 text-2xl font-bold">Habit Together</Text>
         </View>
+        <Pressable className="rounded-2xl bg-stone-200 px-4 py-1 dark:border-2 dark:border-stone-300 dark:bg-stone-800">
+          <Text className="text-base font-semibold">Skip</Text>
+        </Pressable>
+      </View>
 
-        <FlatList
-          data={tutorialData}
-          renderItem={({ item }) => TutorialItem({ item })}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(item) => item.pageNum}
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            {
-              useNativeDriver: false,
-            },
-          )}
-          className="mb-14 grow-0"
-        />
+      <FlatList
+        data={tutorialData}
+        renderItem={({ item }) => TutorialItem({ item })}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.pageNum}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          {
+            useNativeDriver: false,
+          },
+        )}
+        className="mb-14 grow-0"
+      />
 
-        <SlidingDot
-          data={tutorialData}
-          scrollX={scrollX}
-          dotSize={10}
-          containerStyle={{ position: "relative" }}
-          dotStyle={{
-            backgroundColor: colorScheme === "dark" ? colors.white : colors.black,
-          }}
-          marginHorizontal={10}
-          slidingIndicatorStyle={{
-            backgroundColor: colorScheme === "dark" ? colors.white : colors.black,
-          }}
-        />
+      <SlidingDot
+        data={tutorialData}
+        scrollX={scrollX}
+        dotSize={10}
+        containerStyle={{ position: "relative" }}
+        dotStyle={{
+          backgroundColor: colorScheme === "dark" ? colors.white : colors.black,
+        }}
+        marginHorizontal={10}
+        slidingIndicatorStyle={{
+          backgroundColor: colorScheme === "dark" ? colors.white : colors.black,
+        }}
+      />
 
-        <Text className="mb-3 w-2/3 text-center text-xs text-stone-400">
-          By continuing, I agree to the {"\n"}
-          <Link href="/">
-            <Text className="text-xs text-stone-400 underline">
-              Terms & Conditions
-            </Text>
-          </Link>{" "}
-          and{" "}
-          <Link href="/">
-            <Text className="text-xs text-stone-400 underline">
-              Privacy Policy
-            </Text>
-          </Link>
-        </Text>
+      <Text className="mb-3 w-2/3 text-center text-xs text-stone-400">
+        By continuing, I agree to the {"\n"}
+        <Link href="/">
+          <Text className="text-xs text-stone-400 underline">
+            Terms & Conditions
+          </Text>
+        </Link>{" "}
+        and{" "}
+        <Link href="/">
+          <Text className="text-xs text-stone-400 underline">
+            Privacy Policy
+          </Text>
+        </Link>
+      </Text>
 
+      <View className="w-full flex justify-center items-center px-4">
         {Platform.OS === "ios" && (
           <>
             <AppleButton />
@@ -169,41 +162,42 @@ export default function Signin() {
           </>
         )}
       </View>
-    );
+    </View>
+  );
 
-    function GoogleButton() {
-      return (
-        <Pressable
-          className="mt-2 flex w-11/12 flex-row items-center justify-center rounded-xl border-2 p-2 dark:border-stone-300"
-          onPress={signInWithGoogle}
-        >
-          <Image className="h-5 w-5 rounded-[20px]" source={require("../../../assets/images/google.png")} />
-          <Text className="ml-2 text-lg font-semibold">Continue with Google</Text>
-        </Pressable>
-      )
-    }
-
-    function AppleButton() {
-      return (
-        <Pressable
-          className="mt-2 flex w-11/12 flex-row items-center justify-center rounded-xl border-2 p-2 dark:border-stone-300"
-          onPress={signInWithApple}
-        >
-          <IconBrandAppleFilled fill={colors.black} color={colors.black} strokeWidth={0} size={24} />
-          <Text className="ml-2 text-lg font-semibold">Continue with Apple</Text>
-        </Pressable>
-      )
-    }
-
-    function EmailButton() {
-      return (
-        <Pressable
-          className="mt-2 flex w-11/12 flex-row items-center justify-center rounded-xl border-2 p-2 dark:border-stone-300"
-          onPress={signInWithEmail}
-        >
-          <Icon icon={IconMail} lightColor={colors.black} darkColor={colors.white} strokeWidth={2.2} />
-          <Text className="ml-2 text-lg font-semibold">Continue with Email</Text>
-        </Pressable>
-      )
-    }
+  function GoogleButton() {
+    return (
+      <Pressable
+        className="h-14 mt-2 flex w-full flex-row items-center justify-center rounded-xl border-2 p-2 dark:bg-white"
+        onPress={signInWithGoogle}
+      >
+        <Image className="h-5 w-5 rounded-[20px]" source={require("../../../assets/images/google.png")} />
+        <Text className="ml-2 text-lg font-semibold text-black">Continue with Google</Text>
+      </Pressable>
+    )
   }
+
+  function AppleButton() {
+    return (
+      <Pressable
+        className="h-14 mt-2 flex w-full flex-row items-center justify-center rounded-xl border-2 p-2 dark:bg-white"
+        onPress={signInWithApple}
+      >
+        <Image className="h-5 w-5 rounded-[20px]" source={require("../../../assets/images/apple.png")} />
+        <Text className="ml-2 text-lg font-semibold text-black">Continue with Apple</Text>
+      </Pressable>
+    )
+  }
+
+  function EmailButton() {
+    return (
+      <Pressable
+        className="h-14 mt-2 flex w-full  flex-row items-center justify-center rounded-xl border-2 p-2 dark:bg-white"
+        onPress={signInWithEmail}
+      >
+        <Icon icon={IconMail} lightColor={colors.black} darkColor={colors.black} strokeWidth={2.2} />
+        <Text className="ml-2 text-lg font-semibold text-black">Continue with Email</Text>
+      </Pressable>
+    )
+  }
+}
