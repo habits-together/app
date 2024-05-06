f67b2dc3898a90b3d7966e16c94a5cdfdfe7cac3
Ankush Sarkar <ankush.sarkar2002@gmail.com>
2024-05-06 06:46:59 -0400

    added friends list to invite friends

--- src/components/InviteFriends.tsx
+++ src/components/InviteFriends.tsx
@@ -2,6 +2,9 @@
 import { Text, View } from "@/src/components/Themed";
 import { FriendData, getMockFriends } from "@/src/lib/mockData";
 import { useEffect, useState } from "react";
+import FriendCard from "./FriendCard";
+import SmallProfilePicture from "./ProfilePicture";
+import { fetchSingleUserThumbnail } from "../lib/getRandomProfilePics";
 
 export default function InviteFriends() {
   const [friends, setFriends] = useState<FriendData[]>([]);
@@ -9,12 +12,34 @@ export default function InviteFriends() {
     const fetchFriends = async () => {
       const data = await getMockFriends();
       setFriends(data);
+      const pics = await Promise.all(
+        data.map(() => fetchSingleUserThumbnail()),
+      );
+      const updatedFriends = data.map((friend, index) => ({
+        ...friend,
+        profilePicUrl: pics[index].imgurl,
+      }));
+      setFriends(updatedFriends);
     };
     fetchFriends();
   }, []);
   return (
-    <View className="flex-1 p-4">
-        <Text className="text-2xl font-bold">My Friends</Text>
+    <View className="flex-1 p-4" style={{ gap: 0 }}>
+      {friends.length === 1 && (
+        <Text className="text-xl font-bold">My friend</Text>
+      )}
+      {friends.length > 1 && (
+        <Text className="text-xl font-bold">My friends</Text>
+      )}
+      {friends.map((friend) => (
+        <FriendCard
+          key={friend.id}
+          displayName={friend.displayName}
+          userName={friend.userName}
+          profilePic={<SmallProfilePicture picUrl={friend.profilePicUrl} />}
+          commonHabits={friend.commonHabits}
+        />
+      ))}
     </View>
   );
 }
