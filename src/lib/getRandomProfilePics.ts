a9b66ff42f563a4c492e5981585695f967528bd8
owengretzinger <owengretzinger@gmail.com>
2024-05-19 16:30:58 -0400

    Local mock images

--- src/lib/getRandomProfilePics.ts
+++ src/lib/getRandomProfilePics.ts
@@ -1,3 +1,13 @@
+export function getLocalRandomProfilePics(amount: number) {
+  // get pfp from assets/images/mock-pfps 
+  return Array.from({ length: amount }, (_, index) => {
+    return {
+      imgurl: localMockPfps[index % localMockPfps.length],
+      hasCompleted: Math.random() > 0.4 ? true : false,
+    };
+  })
+}
+
 async function fetchUserThumbnails(amount: number) {
   try {
     const response = await fetch(
@@ -43,3 +53,47 @@ export async function fetchSingleUserThumbnail() {
 
 export const profilePicsDataPromise = (amount: number) =>
   fetchUserThumbnails(amount);
+
+
+export const localMockPfps = [
+  require("../../assets/images/mock-pfps/image-1.png"),
+  require("../../assets/images/mock-pfps/image-2.png"),
+  require("../../assets/images/mock-pfps/image-3.png"),
+  require("../../assets/images/mock-pfps/image-4.png"),
+  require("../../assets/images/mock-pfps/image-5.png"),
+  require("../../assets/images/mock-pfps/image-6.png"),
+  require("../../assets/images/mock-pfps/image-7.png"),
+  require("../../assets/images/mock-pfps/image-8.png"),
+  require("../../assets/images/mock-pfps/image-9.png"),
+  require("../../assets/images/mock-pfps/image-10.png"),
+  require("../../assets/images/mock-pfps/image-11.png"),
+  require("../../assets/images/mock-pfps/image-12.png"),
+  require("../../assets/images/mock-pfps/image-13.png"),
+  require("../../assets/images/mock-pfps/image-14.png"),
+  require("../../assets/images/mock-pfps/image-15.png"),
+  require("../../assets/images/mock-pfps/image-16.png"),
+  require("../../assets/images/mock-pfps/image-17.png"),
+  require("../../assets/images/mock-pfps/image-18.png"),
+  require("../../assets/images/mock-pfps/image-19.png"),
+  require("../../assets/images/mock-pfps/image-20.png"),
+  require("../../assets/images/mock-pfps/image-21.png"),
+  require("../../assets/images/mock-pfps/image-22.png"),
+  require("../../assets/images/mock-pfps/image-23.png"),
+  require("../../assets/images/mock-pfps/image-24.png"),
+  require("../../assets/images/mock-pfps/image-25.png"),
+  require("../../assets/images/mock-pfps/image-26.png"),
+  require("../../assets/images/mock-pfps/image-27.png"),
+  require("../../assets/images/mock-pfps/image-28.png"),
+  require("../../assets/images/mock-pfps/image-29.png"),
+  require("../../assets/images/mock-pfps/image-30.png"),
+  require("../../assets/images/mock-pfps/image-31.png"),
+  require("../../assets/images/mock-pfps/image-32.png"),
+  require("../../assets/images/mock-pfps/image-33.png"),
+  require("../../assets/images/mock-pfps/image-34.png"),
+  require("../../assets/images/mock-pfps/image-35.png"),
+  require("../../assets/images/mock-pfps/image-36.png"),
+  require("../../assets/images/mock-pfps/image-37.png"),
+  require("../../assets/images/mock-pfps/image-38.png"),
+  require("../../assets/images/mock-pfps/image-39.png"),
+  require("../../assets/images/mock-pfps/image-40.png"),
+]
