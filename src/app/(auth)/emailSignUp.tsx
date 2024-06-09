afde5f0687daa9459b14f77e729753f538147b41
leungt30 <leungt30@users.noreply.github.com>
2024-06-09 18:22:29 +0000

    Formatted with Prettier (GitHub Action)

--- src/app/(auth)/emailSignUp.tsx
+++ src/app/(auth)/emailSignUp.tsx
@@ -1,54 +1,52 @@
 import AuthButton from "@/src/components/AuthButton";
 import AuthInputField from "@/src/components/AuthInputField";
 import { Text, View } from "@/src/components/Themed";
+import { auth, firestore } from "@/src/firebase/config";
 import { resetNavigationStack } from "@/src/lib/resetNavigationStack";
 import { Link } from "expo-router";
-import {auth,firestore} from "@/src/firebase/config";
-import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
 
 export default function emailsignup() {
-  
-  const [data,setData] = useState({
-    email: '',
-    password: '',
+  const [data, setData] = useState({
+    email: "",
+    password: "",
   });
-  
+
   const handleEmailChange = (email: string) => {
-    setData({...data,email: email});
-  }
+    setData({ ...data, email: email });
+  };
 
   const handlePasswordChange = (password: string) => {
-    setData({...data,password: password});
-  }
-
+    setData({ ...data, password: password });
+  };
 
   const SignUp = () => {
     console.log("Sign Up Attempted");
     createUserWithEmailAndPassword(auth, data.email, data.password).then(
-      success => {
+      (success) => {
         handleDatabaseSignUp();
         resetNavigationStack("/");
       },
-      err => {
-        switch(err.code) {
-          case 'auth/email-already-in-use':
-            alert("Email already in use");  
+      (err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+            alert("Email already in use");
             return;
-          case 'auth/invalid-email':
+          case "auth/invalid-email":
             alert("Invalid email");
             return;
-          case 'auth/weak-password':
+          case "auth/weak-password":
             alert("Weak password");
             return;
           default:
             alert(`An error occurred. Please try again. ${err.code}`);
             return;
         }
-      });
+      },
+    );
   };
-
 
   const handleDatabaseSignUp = async () => {
     // BASE DATA
@@ -61,14 +59,14 @@ export default function emailsignup() {
       nudge_list: [],
       picture: "",
       friend_id: auth.currentUser?.email,
-    }
+    };
 
     const dummyHabitData = {
       completion_date: new Date(),
       habit_id: "BASEHABIT",
       times_completed: 0,
-    }
-    
+    };
+
     // PUSH TO FIREBASE
     if (auth.currentUser) {
       // write regular user data doc
@@ -76,26 +74,37 @@ export default function emailsignup() {
       await setDoc(docRef, baseData);
 
       // create habit completion collection
-      const habitUserCompletionColRef = collection(docRef, "habits_subscribed_to_completions");
-      const id = "BASEHABIT-" + String(new Date().toLocaleDateString('en-CA').replace(/\//g, '')) //BASEHABIT-YYYYMMDD
+      const habitUserCompletionColRef = collection(
+        docRef,
+        "habits_subscribed_to_completions",
+      );
+      const id =
+        "BASEHABIT-" +
+        String(new Date().toLocaleDateString("en-CA").replace(/\//g, "")); //BASEHABIT-YYYYMMDD
       const habitCompletetionDocRef = doc(habitUserCompletionColRef, id);
       await setDoc(habitCompletetionDocRef, dummyHabitData);
-      
 
-
-      console.log("Added User Document written with ID: ", auth.currentUser.uid);
-    }
-    else { //potential issue: if user gets created but DB write fails
+      console.log(
+        "Added User Document written with ID: ",
+        auth.currentUser.uid,
+      );
+    } else {
+      //potential issue: if user gets created but DB write fails
       console.log("An error occurred. Please try again.");
     }
-
-    
-
-  }
+  };
   return (
     <View className="flex-1 items-center px-3 pt-5">
-      <AuthInputField text="Email" isPass={false} updateFunction={handleEmailChange}/>
-      <AuthInputField text="Password" isPass={true} updateFunction={handlePasswordChange}/>
+      <AuthInputField
+        text="Email"
+        isPass={false}
+        updateFunction={handleEmailChange}
+      />
+      <AuthInputField
+        text="Password"
+        isPass={true}
+        updateFunction={handlePasswordChange}
+      />
       <Text className="mb-5 w-2/3 text-center text-xs text-stone-400">
         By continuing, I agree to the {"\n"}
         <Link href="/">
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,7 @@
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 // authentication
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 // firestore
 import { getFirestore } from "firebase/firestore";
 // cloud storage
@@ -22,7 +21,7 @@ const app = initializeApp(firebaseConfig);
 
 // react native persistence should allow the app to remember the user's login state
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage), 
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 export const firestore = getFirestore(app);
 export const storage = getStorage();
