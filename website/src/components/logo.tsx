946d7daed8d2aa8b1ef08d826deda65e3866d5c6
Owen Gretzinger <owengretzinger@gmail.com>
2024-06-15 09:59:08 -0400

    [HAB-134] SVG logo component (#67)
    Co-authored-by: owengretzinger <owengretzinger@users.noreply.github.com>

---
+++ website/src/components/logo.tsx
@@ -0,0 +1,42 @@
+export default function Logo() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.51428 14.7428L1.02856 17.4857L12 22.9714L22.9714 17.4857L17.4857 14.7428"
+        fill="#60A5FA"
+      />
+      <path
+        d="M6.51428 14.7428L1.02856 17.4857L12 22.9714L22.9714 17.4857L17.4857 14.7428"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.51428 9.2572L1.02856 12.0001L12 17.4858L22.9714 12.0001L17.4857 9.2572"
+        fill="#34D399"
+      />
+      <path
+        d="M6.51428 9.2572L1.02856 12.0001L12 17.4858L22.9714 12.0001L17.4857 9.2572"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 1.02856L1.02856 6.51428L12 12L22.9714 6.51428L12 1.02856Z"
+        fill="#FB923C"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
