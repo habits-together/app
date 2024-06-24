ec078254159d8be464f612cdb674575b6035cfe8
owengretzinger <owengretzinger@gmail.com>
2024-06-23 20:50:13 -0400

    Add metadata

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,9 +4,36 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title =
+  "Habits Together - Social Habit Tracking App";
+const description =
+  "Habits Together is the perfect app to help you and your friends stay accountable and motivated to reach your goals. Sign up and we'll let you know when the app is ready!";
+
 export const metadata: Metadata = {
-  title: "Habits Together - Add friends add do habits together",
-  description: "Sign up and we'll let you know when the app is ready!",
+  title: title,
+  description: description,
+
+  openGraph: {
+    title: title,
+    description: description,
+    url: "https://habits-together.vercel.app/",
+    images: {
+      url: "/opengraph-image.png",
+      alt: "Habits are hard. Habits Together is the perfect app to help you and your friends stay accountable and motivated to reach your goals.",
+    },
+    locale: "en_US",
+    type: "website",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  metadataBase: new URL("https://habits-together.vercel.app/"),
+  alternates: {
+    canonical: "/",
+  },
 };
 
 export default function RootLayout({
