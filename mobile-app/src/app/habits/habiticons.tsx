ad5448b1c21b801593e4481e5100cb471f8e6f73
Hilary He <97636372+HilaryHe1012@users.noreply.github.com>
2024-07-02 21:28:02 -0400

    [HAB-143] Add all icons (#85)
    Co-authored-by: Geon Youn <geon.youn@outlook.com>
    Co-authored-by: HilaryHe1012 <HilaryHe1012@users.noreply.github.com>

--- mobile-app/src/app/habits/habiticons.tsx
+++ mobile-app/src/app/habits/habiticons.tsx
@@ -5,26 +5,183 @@ import { IconChevronLeft } from "@tabler/icons-react-native";
 import { router } from "expo-router";
 import { useAtom } from "jotai";
 import { useMemo } from "react";
-import { TouchableOpacity } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native";
 import { IconButton } from "./icon-button";
 import { iconStrNameToTablerIcon } from "./icons";
 
 type HabitIconsTuple = [string, string[]];
 
+const categories: HabitIconsTuple[] = [
+  [
+    "Health & Fitness",
+    [
+      "barbell",
+      "yoga",
+      "walk",
+      "run",
+      "swimming",
+      "bike",
+      "bed",
+      "leaf",
+      "bottle",
+      "salad",
+      "apple",
+      "activity",
+      "heartbeat",
+      "AdjustmentsHeart",
+    ],
+  ],
+  [
+    "Productivity & Work",
+    [
+      "deviceLaptop",
+      "clock",
+      "calendar",
+      "briefcase",
+      "edit",
+      "chartDots3",
+      "mail",
+      "table",
+      "deviceDesktop",
+      "fileText",
+      "boxMultiple",
+      "checklist",
+      "buildingEstate",
+      "moodEdit",
+    ],
+  ],
+  [
+    "Personal Development",
+    [
+      "brain",
+      "bulb",
+      "ladder",
+      "school",
+      "deviceMobileOff",
+      "code",
+      "language",
+      "microscope",
+      "certificate",
+      "books",
+      "listSearch",
+      "jewishStar",
+      "trendingUp",
+      "chartArrowsVertical",
+    ],
+  ],
+  [
+    "Leisure & Recreation",
+    [
+      "brush",
+      "music",
+      "camera",
+      "book",
+      "chess",
+      "headphones",
+      "deviceGamepad2",
+      "movie",
+      "beach",
+      "tent",
+      "fishHook",
+      "chefHat",
+      "rollerSkating",
+      "video",
+      "ballBasketball",
+      "ballVolleyball",
+      "ballTennis",
+      "ballBaseball",
+      "ballAmericanFootball",
+      "ballFootball",
+      "pingPong",
+    ],
+  ],
+  [
+    "Social & Family",
+    [
+      "usersGroup",
+      "users",
+      "userHeart",
+      "moodHeart",
+      "moodCrazyHappy",
+      "heart",
+      "hearts",
+      "heartHandshake",
+      "home",
+      "homeHeart",
+      "messageChatbot",
+      "messages",
+      "worldHeart",
+      "confetti",
+    ],
+  ],
+  [
+    "Mindfullness & Spirituality",
+    [
+      "peace",
+      "buildingChurch",
+      "window",
+      "flower",
+      "michelinStar",
+      "planet",
+      "empathize",
+      "plant2",
+      "plant",
+      "trees",
+      "hourglass",
+      "scale",
+      "yinYang",
+      "moon2",
+    ],
+  ],
+  [
+    "Financial",
+    [
+      "buildingBank",
+      "wallet",
+      "chartLine",
+      "reportMoney",
+      "databaseDollar",
+      "coin",
+      "coins",
+      "pigMoney",
+      "homeStats",
+      "zoomMoney",
+      "devicesDollar",
+      "deviceAnalytics",
+      "shoppingCartDollar",
+      "moodDollar",
+    ],
+  ],
+];
+
 export default function HabitIcons() {
   const [icon, setIcon] = useAtom(tempIconAtom);
 
   const habitIcons: HabitIconsTuple[] = useMemo(() => {
-    return [
-      ["Health & Fitness", ["barbell", "yoga", "leaf"]],
-      ["Productivity & Work", ["deviceLaptop", "clock", "calendar"]],
-      ["Personal Development", ["brain", "bulb", "ladder"]],
-      ["Leisure & Recreation", ["brush", "music", "camera"]],
-      ["Social & Family", ["usersGroup", "heart", "home"]],
-      ["Mindfullness & Spirituality", ["peace", "buildingChurch", "flower"]],
-      ["Financial", ["buildingBank", "wallet", "chartLine"]],
-    ];
+    return categories;
   }, []);
+  const renderItem = ({ item }: { item: HabitIconsTuple }) => {
+    const [section, keys] = item;
+    return (
+      <View className="mb-5" key={section}>
+        <Text className="text-base font-semibold">{section}</Text>
+        <View className="flex-row flex-wrap">
+          {keys.map((key) => (
+            <View className="mr-2 mt-2" key={key}>
+              <IconButton
+                icon={iconStrNameToTablerIcon(key)}
+                onPress={() => {
+                  setIcon(key);
+                  router.back();
+                }}
+                selected={icon === key}
+              />
+            </View>
+          ))}
+        </View>
+      </View>
+    );
+  };
 
   return (
     <View className="flex flex-1 flex-col gap-y-5 px-5 pt-12">
@@ -41,30 +198,13 @@ export default function HabitIcons() {
         </TouchableOpacity>
         <Text className="text-base font-semibold">Select Icon</Text>
       </View>
-      {habitIcons.map((item: HabitIconsTuple) => {
-        const [section, keys] = item;
-        return (
-          <View key={section}>
-            <Text className="text-base font-semibold">{section}</Text>
-            <View className="flex flex-row">
-              {keys.map((key) => {
-                return (
-                  <View className="mr-2 mt-2" key={key}>
-                    <IconButton
-                      icon={iconStrNameToTablerIcon(key)}
-                      onPress={() => {
-                        setIcon(key);
-                        router.back();
-                      }}
-                      selected={icon === key}
-                    />
-                  </View>
-                );
-              })}
-            </View>
-          </View>
-        );
-      })}
+
+      {/* Content */}
+      <FlatList
+        data={habitIcons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item[0]}
+      />
     </View>
   );
 }
