e6ff7035e65a47d711f647192f4573dcabda8160
Owen Gretzinger <owengretzinger@gmail.com>
2025-01-11 12:50:43 -0500

    feat: view habit page; ability to view everyone's entries

--- src/components/modify-habit-entry/day-navigation.tsx
+++ src/components/modify-habit-entry/day-navigation.tsx
@@ -1,19 +1,18 @@
 import { ArrowLeftIcon, ArrowRightIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
 
-import { type HabitCompletionWithDateInfoT } from '@/api';
 import { Pressable, Text, View } from '@/ui';
 
 interface DayNavigationProps {
   selectedDayIndex: number;
-  completions: HabitCompletionWithDateInfoT[];
+  datesList: Date[];
   hasUnsavedChanges: boolean;
   onDayChange: (index: number) => void;
   resetStates: () => void;
 }
 export function DayNavigation({
   selectedDayIndex,
-  completions,
+  datesList,
   hasUnsavedChanges,
   onDayChange,
   resetStates,
@@ -22,55 +21,59 @@ export function DayNavigation({
   return (
     <View className="flex-col gap-2">
       <View className="flex-row items-center justify-center">
-        <Pressable
-          className="rounded-lg border border-slate-200 px-4 py-2 dark:border-stone-700 dark:bg-transparent"
-          onPress={() => {
-            onDayChange(selectedDayIndex - 1);
-            resetStates();
-          }}
-          disabled={selectedDayIndex === 0 || hasUnsavedChanges}
-          style={{
-            opacity: selectedDayIndex === 0 || hasUnsavedChanges ? 0.3 : 1,
-          }}
-        >
-          <ArrowLeftIcon
-            size={16}
-            color={colorScheme === 'dark' ? 'white' : 'black'}
-          />
-        </Pressable>
+        {datesList.length > 1 && (
+          <Pressable
+            className="rounded-lg border border-slate-200 bg-white px-4 py-2 dark:border-stone-700 dark:bg-transparent"
+            onPress={() => {
+              onDayChange(selectedDayIndex - 1);
+              resetStates();
+            }}
+            disabled={selectedDayIndex === 0 || hasUnsavedChanges}
+            style={{
+              opacity: selectedDayIndex === 0 || hasUnsavedChanges ? 0.3 : 1,
+            }}
+          >
+            <ArrowLeftIcon
+              size={16}
+              color={colorScheme === 'dark' ? 'white' : 'black'}
+            />
+          </Pressable>
+        )}
         <Text className="w-32 text-center font-bold">
           {new Date(
-            new Date(completions[selectedDayIndex].date).setDate(
-              new Date(completions[selectedDayIndex].date).getDate() + 1,
+            Date.UTC(
+              datesList[selectedDayIndex].getUTCFullYear(),
+              datesList[selectedDayIndex].getUTCMonth(),
+              datesList[selectedDayIndex].getUTCDate(),
             ),
           ).toLocaleDateString('en-US', {
             weekday: 'short',
             month: 'short',
             day: 'numeric',
+            timeZone: 'UTC',
           })}
         </Text>
-        <Pressable
-          className="rounded-lg border border-slate-200 px-4 py-2 dark:border-stone-700 dark:bg-transparent"
-          onPress={() => onDayChange(selectedDayIndex + 1)}
-          disabled={
-            selectedDayIndex === completions.length - 1 || hasUnsavedChanges
-          }
-          style={{
-            opacity:
-              selectedDayIndex === completions.length - 1 || hasUnsavedChanges
-                ? 0.3
-                : 1,
-          }}
-        >
-          <ArrowRightIcon
-            size={16}
-            color={colorScheme === 'dark' ? 'white' : 'black'}
-          />
-        </Pressable>
+        {datesList.length > 1 && (
+          <Pressable
+            className="rounded-lg border border-slate-200 bg-white px-4 py-2 dark:border-stone-700 dark:bg-transparent"
+            onPress={() => onDayChange(selectedDayIndex + 1)}
+            disabled={
+              selectedDayIndex === datesList.length - 1 || hasUnsavedChanges
+            }
+            style={{
+              opacity:
+                selectedDayIndex === datesList.length - 1 || hasUnsavedChanges
+                  ? 0.3
+                  : 1,
+            }}
+          >
+            <ArrowRightIcon
+              size={16}
+              color={colorScheme === 'dark' ? 'white' : 'black'}
+            />
+          </Pressable>
+        )}
       </View>
-      <Text className="text-center text-sm text-orange-500 dark:text-orange-500">
-        {hasUnsavedChanges ? 'You have unsaved changes' : ' '}
-      </Text>
     </View>
   );
 }
